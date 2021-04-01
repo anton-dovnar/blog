b603360f669ce53d262787f8201c23baf1210fee
Anton Dovnar <fode4cun@gmail.com>
2021-04-01 14:37:20 +0300

    TDD pass - add paginator for post list

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     context_object_name = 'post_list'
+    paginate_by = 5
     template_name = 'blog/post_list.html'
 
 
