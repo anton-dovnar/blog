6094e969f9c7515e7005e078f5e03eed756cb072
Anton Dovnar <fode4cun@gmail.com>
2021-04-01 12:15:09 +0300

    TDD create post model / pass tests

---
+++ blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique_for_date='published')
+    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-published',)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(**kwargs)
