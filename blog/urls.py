e64f8ea0c9791186eb037692f2025ee2186fdabc
Anton Dovnar <fode4cun@gmail.com>
2021-04-02 23:46:05 +0300

    Create PostShare view for sharing posts via email

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostList.as_view(), name='post-list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.PostDetail.as_view(), name='post-detail'),
+    path('<int:post_pk>/share/', views.PostShare.as_view(), name='post-share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views import generic
+from django.views.decorators.debug import sensitive_post_parameters
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -26,3 +31,17 @@ class PostDetail(generic.DetailView):
             published__day=self.kwargs['day']
         )
         return post
+
+
+@method_decorator(sensitive_post_parameters(), name='post')
+class PostShare(SuccessMessageMixin, generic.FormView):
+    form_class = EmailPostForm
+    template_name = 'blog/post_share.html'
+    success_url = reverse_lazy('blog:post-list')
+    success_message = 'Mail sent successfully'
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['post_pk'])
+        post_url = self.request.build_absolute_uri(post.get_absolute_url())
+        form.send(post, post_url)
+        return super().form_valid(form)
