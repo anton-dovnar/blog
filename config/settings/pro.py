278684f049b6a9b2c93f36eb9cf40b2f5d0b0a74
Anton Dovnar <fode4cun@gmail.com>
2021-04-13 23:46:31 +0300

    Set up production settings for deploying to heroku

--- config/settings/pro.py
+++ config/settings/pro.py
@@ -1,1 +1,27 @@
 from .base import *
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+DATABASES = {
+    "default": env.dj_db_url("DATABASE_URL")
+}
+
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 2592000
+SECURE_HSTS_PRELOAD = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
